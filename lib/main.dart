bb61074483336e1d660d63aa789afa26f6189f5f
Sheyla Cantalupo <sheylacantalupo.sc@gmail.com>
2024-01-21 17:19:29 -0300

    stateless-stateful

--- lib/main.dart
+++ lib/main.dart
@@ -16,87 +16,139 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // o stack permite empilhar os containers
-      home: Container(
-        color: Colors.white60,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      // Scaffold é a estrutura completa do app
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          title: Text(
+            'Tarefas',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: ListView(
+          // essa já é a configuração default
+          scrollDirection: Axis.vertical,
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 50,
-                  height: 50,
-                ),
-              ],
-            ),
-            Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                Container(
-                  color: Colors.blue,
-                  width: 100,
-                  height: 100,
-                ),
-                Container(
-                  color: Colors.red,
-                  width: 50,
-                  height: 50,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.blue,
-                  width: 20,
-                  height: 20,
-                ),
-                Container(
-                  color: Colors.red,
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 20,
-                  height: 20,
-                ),
-                Container(
-                  color: Colors.red,
-                  width: 50,
-                  height: 50,
-                ),
-              ],
-            ),
+            Task('Aprender Flutterrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr'),
+            Task('Andar de bike'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+            Task('Meditar'),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
+    );
+  }
+}
+
+class Task extends StatefulWidget {
+  final String nome;
+
+  const Task(this.nome, {super.key});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
             Container(
-              color: Colors.amber,
-              height: 30,
-              width: 300,
-              child: Text(
-                'My container',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
-                ),
-                textAlign: TextAlign.center,
-              ),
+              color: Colors.blue,
+              height: 140,
             ),
-            ElevatedButton(
-              onPressed: () {
-                print('Você apertou o botão');
-              },
-              child: Text('Aperte o botão!'),
-            )
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Container(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          )),
+                      Container(
+                        height: 52,
+                        //width: 52,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+                              print(nivel);
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              //crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(
+                                  Icons.arrow_drop_up,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                  'UP',
+                                  style: TextStyle(
+                                      fontSize: 12, color: Colors.white),
+                                ),
+                              ],
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          backgroundColor: Colors.blue.shade200,
+                          value: nivel / 10,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('Nível: $nivel',
+                          style: TextStyle(color: Colors.white)),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
---
+++ lib/primeira_tela.dart
@@ -0,0 +1,87 @@
+// Container
+// (
+// color: Colors.white60,
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(
+// color: Colors.red,
+// width: 100,
+// height: 100,
+// ),
+// Container(
+// color: Colors.blue,
+// width: 50,
+// height: 50,
+// ),
+// ],
+// ),
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(
+// color: Colors.blue,
+// width: 100,
+// height: 100,
+// ),
+// Container(
+// color: Colors.red,
+// width: 50,
+// height: 50,
+// ),
+// ],
+// ),
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Container(
+// color: Colors.blue,
+// width: 20,
+// height: 20,
+// ),
+// Container(
+// color: Colors.red,
+// width: 50,
+// height: 50,
+// ),
+// Container(
+// color: Colors.blue,
+// width: 20,
+// height: 20,
+// ),
+// Container(
+// color: Colors.red,
+// width: 50,
+// height: 50,
+// ),
+// ],
+// ),
+// Container(
+// color: Colors.amber,
+// height: 30,
+// width: 300,
+// child: Text(
+// 'My container',
+// style: TextStyle(
+// color: Colors.black,
+// fontSize: 26,
+// ),
+// textAlign: TextAlign.center,
+// ),
+// ),
+// ElevatedButton(
+// onPressed: () {
+// print('Você apertou o botão');
+// },
+// child: Text('Aperte o botão!'),
+// )
+// ]
+// ,
+// )
+// ,
+// )
